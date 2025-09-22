6a7ea81034b813b75c9fbcd679e4f18d6a27067c
muzzi <muzammilahmed0888@gmail.com>
2025-09-23 02:42:47 +0500

    Update

---
+++ Lab/Lab/Lab/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Lab_1Task_2 a = new Lab_1Task_2();
+        a.calculate();
+        a.display();
+    }
+}
