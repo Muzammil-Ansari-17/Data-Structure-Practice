ce8a20a212cf2423ccf9b0f6ac777bbb25bec3a9
muzzi <muzammilahmed0888@gmail.com>
2025-09-26 15:49:06 +0500

    feat update

---
+++ Practice/Practice/src/main/java/org/example/TwodArrray.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class TwodArrray {
+
+    Scanner sc = new Scanner(System.in);
+    int row;
+    int coloum;
+
+    public void input(){
+
+        System.out.println("Enter Rows :");
+        row = sc.nextInt();
+        System.out.println("Enter Coloums :");
+        coloum = sc.nextInt();
+        int[][] arr = new int[row][coloum];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < coloum; j++) {
+                System.out.print("Enter Elemnt of Array"+i+" "+j+" :");
+                arr[i][j] = sc.nextInt();
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < coloum; j++) {
+                System.out.println(arr[i][j]);
+            }
+        }
+        System.out.println();
+    }
+}
