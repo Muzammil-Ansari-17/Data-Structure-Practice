053d598a79879b9cc8be6c9d65292fcdbb2a997d
muzzi <muzammilahmed0888@gmail.com>
2025-09-29 10:44:25 +0500

    feat update

---
+++ Practice/Practice/src/main/java/org/example/TwoSum.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,5,6,7};
+        int target = 7;
+
+        public int[] solution(arr , target){
+
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = i+1; j < arr.length ; j++) {
+                    if(arr[i] + arr[j] == target){
+                        return int[]{i,j};
+                    }
+                }
+            }
+        }
+
+
+    }
+}
