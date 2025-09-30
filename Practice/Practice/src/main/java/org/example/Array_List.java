749ca63f1d719560e800d912c089c8ddafc37606
muzzi <muzammilahmed0888@gmail.com>
2025-09-30 20:47:10 +0500

    feat update

--- Practice/Practice/src/main/java/org/example/Array_List.java
+++ Practice/Practice/src/main/java/org/example/Array_List.java
@@ -2,40 +2,41 @@ package org.example;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import static java.util.Collections.binarySearch;
+
 public class Array_List {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
 
-        list.add(1);
-        list.add(2);
-        list.add(3);
+        list.add(19);
+        list.add(26);
+        list.add(93);
         list.add(25);
         list.add(100);
+        list.add(26);
+        list.add(66);
+        list.add(99);
+        list.add(76);
+        list.add(-3);
+        list.add(95);
+        list.add(0);
+        list.add(-26);
+        list.add(56);
+        list.add(-6);
+        list.add(-83);
+        list.add(65);
+        list.add(40);
+        list.add(16);
+        list.add(46);
+
         System.out.println(list);
-
-        int element = list.get(2);
-        System.out.println(element);
-
-        list.addFirst(10);
-        list.add(4, -1);
-        System.out.println(list);
-
-        list.set(0, 99);
-        System.out.println(list);
-
-        list.remove(3);
-        System.out.println(list);
-
-        int size = list.size();
-        System.out.println(size);
-
-        for (Integer integer : list) {
-            System.out.println(integer);
-        }
-
 
         Collections.sort(list);
         System.out.println(list);
+
+
+
+
 
     }
 }
--- Practice/Practice/src/main/java/org/example/Main.java
+++ Practice/Practice/src/main/java/org/example/Main.java
@@ -1,13 +1,15 @@
 package org.example;
 
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-    Dynamic_Array ar = new Dynamic_Array();
-        ar.insert(5);
-        System.out.println(ar);
+    Arrayinput A = new Arrayinput();
+    A.insert(5);
+
 
     }
 }
