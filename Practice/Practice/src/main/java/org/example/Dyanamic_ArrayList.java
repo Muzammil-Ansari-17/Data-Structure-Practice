4c4e94ce1d4d21b6fdfed01fc293fe69dfc637f3
muzzi <muzammilahmed0888@gmail.com>
2025-10-01 13:06:33 +0500

    feat update

--- Practice/Practice/src/main/java/org/example/Dyanamic_ArrayList.java
+++ Practice/Practice/src/main/java/org/example/Dyanamic_ArrayList.java
@@ -1,7 +1,6 @@
 package org.example;
-import java.lang.reflect.InvocationTargetException;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
 
 public class Dyanamic_ArrayList {
